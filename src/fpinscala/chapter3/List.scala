29632e2537aee3ea8325f374132b621a7f68ebb6
Chenyu Zhang <chenyuzhcy@gmail.com>
2017-08-17 22:25:48 -0400

    exercise

--- src/fpinscala/chapter3/List.scala
+++ src/fpinscala/chapter3/List.scala
@@ -54,4 +54,47 @@ object List {
     case Cons(_, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(h, t) => f(h, foldRight(t, z)(f))
+    }
+
+  // 3.9
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, acc) => acc + 1)
+
+  // 3.10
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => acc
+      case Cons(h, t) => foldLeft(t, f(acc, h))
+    }
+  }
+
+  // 3.11
+  def sum2(ints: List[Int]): Int =
+    foldLeft(ints, 0)((acc, h) => acc + h)
+
+  def product2(ds: List[Double]): Double =
+    foldLeft(ds, 1.0)((acc, h) => acc * h)
+
+  def length2[A](as: List[A]): Int =
+    foldLeft(as, 0)((acc, _) => acc + 1)
+
+  // 3.12
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil:List[A])((acc, x) => Cons(x, acc))
+
+  // 3.13
+
+  // 3.14
+  def append[A](l1: List[A], l2: List[A]): List[A] =
+    foldRight(l1, l2)((x, acc) => Cons(x, acc))
+
+  // 3.15
+  def concat[A](l: List[List[A]]): List[A] =
+    foldLeft(l, Nil:List[A])((acc, x) => append(acc, x))
 }
