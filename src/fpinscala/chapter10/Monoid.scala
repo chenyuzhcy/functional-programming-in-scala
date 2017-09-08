4e39bd2e69396e88d0e9c5466202e362f29111db
Chenyu Zhang <chenyuzhcy@gmail.com>
2017-09-07 22:10:00 -0400

    exercise

--- src/fpinscala/chapter10/Monoid.scala
+++ src/fpinscala/chapter10/Monoid.scala
@@ -58,6 +58,24 @@ object Monoid {
 
   // 10.9
 
+  // 10.16
+  def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
+    new Monoid[(A, B)] {
+      def op(x: (A, B), y: (A, B)): (A, B) = (A.op(x._1, y._1), B.op(x._2, y._2))
+      def zero: (A, B) = (A.zero, B.zero)
+    }
+
+  // 10.17
+  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] =
+    new Monoid[(A) => B] {
+      override def op(x: (A) => B, y: (A) => B): (A) => B =
+        a => B.op(x(a), y(a))
+      override def zero: (A) => B =
+        _ => B.zero
+    }
+
+  // 10.18
+
 }
 
 sealed trait WC
@@ -67,6 +85,7 @@ case class Part(lStub: String, words: Int, rStub: String) extends WC
 object WCMonoid {
   import Monoid._
 
+  // 10.10
   val wcMonoid: Monoid[WC] = new Monoid[WC] {
     def op(a1: WC, a2: WC): WC = {
       (a1, a2) match {
@@ -82,6 +101,7 @@ object WCMonoid {
     def zero: WC = Stub("")
   }
 
+  // 10.11
   def countWords(s: String): Int = {
     def toWC(c: Char): WC =
       if (c.isWhitespace) Part("", 0, "")
@@ -101,7 +121,47 @@ object WCMonoid {
 trait Foldable[F[_]] {
   def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
   def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
-  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+    foldLeft(as)(mb.zero)((b, a) => mb.op(b, f(a)))
   def concatenate[A](as: F[A])(m: Monoid[A]): A =
     foldLeft(as)(m.zero)(m.op)
+  // 10.15
+  def toList[A](fa: F[A]): List[A] =
+    foldRight(fa)(List[A]())((a, b) => a::b)
+}
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Foldable {
+  // 10.12
+  val foldableList = new Foldable[List] {
+    def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+  }
+
+  // 10.13
+  val foldableTree = new Foldable[Tree] {
+    def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B =
+      as match {
+        case Leaf(a) => f(a, z)
+        case Branch(l, r) => foldRight(l)(foldRight(r)(z)(f))(f)
+      }
+
+    def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B =
+      as match {
+        case Leaf(a) => f(z, a)
+        case Branch(l, r) => foldLeft(l)(foldLeft(r)(z)(f))(f)
+      }
+  }
+
+  // 10.14
+  val foldableOption = new Foldable[Option] {
+    def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B =
+      as.foldRight(z)(f)
+    def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B): B =
+      as.foldLeft(z)(f)
+  }
+
 }
---
+++ src/fpinscala/chapter11/Monad.scala
@@ -0,0 +1,45 @@
+package fpinscala.chapter11
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(x => x._1), map(fab)(x => x._2))
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] =
+    e match {
+      case Left(fa) => map(fa)(a => Left(a))
+      case Right(fb) => map(fb)(b => Right(b))
+    }
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}
+
+object Monad {
+  // 11.1
+    // Par
+
+    // Parser
+
+  val optionMonad = new Monad[Option] {
+    def unit[A](a: => A): Option[A] = Some(a)
+    def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] = ma flatMap f
+  }
+
+  val streamMonad = new Monad[Stream] {
+    def unit[A](a: => A): Stream[A] = Stream(a)
+    def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] = ma flatMap f
+  }
+
+  val listMonad = new Monad[List] {
+    def unit[A](a: => A): List[A] = List(a)
+    def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma flatMap f
+  }
+
+}
