6928043cffe44daef503252674ce22708ce4c6fd
Chenyu Zhang <chenyuzhcy@gmail.com>
2018-11-18 21:42:02 -0500

    applicative exercise

---
+++ src/fpinscala/chapter12/Applicative.scala
@@ -0,0 +1,42 @@
+package fpinscala.chapter12
+
+import fpinscala.chapter11.Functor
+
+trait Applicative[F[_]] extends Functor[F] {
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def unit[A](a: => A): F[A]
+
+  // derived
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    map2(fa, unit(()))((a, _) => f(a))
+
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, flb) => map2(f(a), flb)(_::_))
+
+  // 12.1
+  def sequence[A](fas: List[F[A]]): F[List[A]] =
+    traverse(fas)(identity)
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+    sequence(List.fill(n)(fa))
+
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((a, b) => (a, b))
+
+  // 12.2
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)((f, a) => f(a))
+
+  // 12.3
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = {
+    val fbc = apply(map(fa)(f.curried))(fb)
+    apply(fbc)(fc)
+  }
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+    val fbcd = apply(map(fa)(f.curried))(fb)
+    val fcd = apply(fbcd)(fc)
+    apply(fcd)(fd)
+  }
+  
+}
